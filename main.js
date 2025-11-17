24e867826908b5200274b108f3b80e7e4a7b3006
ebosebastien68-maker <ebosebastien68@gmail.com>
2025-11-17 22:32:40 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+import Welcome from './components/Welcome.js';
+
+// Récupère le container principal
+const app = document.getElementById('app');
+
+// Affiche le composant Welcome
+app.appendChild(Welcome());
