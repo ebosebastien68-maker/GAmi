9d344169cecd39b0e666e8f6d8195843c11fce1b
ebosebastien68-maker <ebosebastien68@gmail.com>
2025-11-19 14:57:58 +0100

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-import App from './App.js'; // Le “cerveau” qui gère tous les composants
-
-// Récupère le container principal
-const app = document.getElementById('app');
-
-// Monte l'application
-App(app);
